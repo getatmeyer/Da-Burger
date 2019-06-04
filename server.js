67b5b8d240b7a6b2b9daea5c2a8a6b1fcf635828
nathan meyer <nmeyer@nathans-MacBook-Air.local>
2019-06-04 16:06:11 -0700

    <updates>

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static("public/assets/css"))
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +22,11 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
+
+// app.use(express.static("./public/assets/css"));
+app.use(express.static("public/assets/css"));
+// app.use(express.static("./public"));
+// console.log("This is being rendered" + express.static); ERROR
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
